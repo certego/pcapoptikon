e379c1d31443e52023c3bf59404e36fd159df1d4
Pietro Delsante <pietro.delsante@gmail.com>
2015-08-11 17:50:45 +0200

    Also adding SessionAuthentication

--- main/api.py
+++ main/api.py
@@ -1,5 +1,5 @@
 from tastypie.resources import ModelResource, ALL, ALL_WITH_RELATIONS
-from tastypie.authentication import BasicAuthentication, ApiKeyAuthentication, MultiAuthentication
+from tastypie.authentication import BasicAuthentication, ApiKeyAuthentication, SessionAuthentication, MultiAuthentication
 from pcapoptikon.authorization import CertegoDjangoAuthorization
 from pcapoptikon.fields import Base64FileField
 from main.models import *
@@ -14,7 +14,7 @@ class TaskResource(ModelResource):
         queryset = Task.objects.all().order_by('-id')
         resource_name = 'task'
         allowed_methods = ['get', 'post']
-        authentication = MultiAuthentication(BasicAuthentication(), ApiKeyAuthentication())
+        authentication = MultiAuthentication(BasicAuthentication(), ApiKeyAuthentication(), SessionAuthentication())
         authorization = CertegoDjangoAuthorization()
         filtering = {
             'submitted_on': ALL,
