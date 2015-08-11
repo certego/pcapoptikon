59eb2dbf67cba817cd1ba58b2c70150a7915076c
Pietro Delsante <p.delsante@certego.net>
2015-08-11 10:46:20 +0000

    Using MySQL as the backend database

--- pcapoptikon/settings.py
+++ pcapoptikon/settings.py
@@ -74,8 +74,13 @@ WSGI_APPLICATION = 'pcapoptikon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pcapoptikon',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+        'OPTIONS': { "init_command": "SET storage_engine=INNODB, SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED", },
     }
 }
 
