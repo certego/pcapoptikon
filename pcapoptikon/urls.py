22162e13fc593da89ece685dabf9eb713505b3f3
Pietro Delsante <pietro.delsante@gmail.com>
2015-08-11 13:05:30 +0200

    Adding API urls

--- pcapoptikon/urls.py
+++ pcapoptikon/urls.py
@@ -1,5 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+admin.autodiscover()
+
+from tastypie.api import Api
+from main.api import *
+
+v1_api = Api(api_name='v1')
+v1_api.register(TaskResource())
 
 urlpatterns = patterns('',
     # Examples:
