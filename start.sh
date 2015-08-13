5ac41c7ff4f82dae92d0ea2c4e495b01e0505653
Pietro Delsante <pietro.delsante@gmail.com>
2015-08-13 19:05:06 +0200

    Allow to run in daemon mode

--- start.sh
+++ start.sh
@@ -7,3 +7,4 @@ python manage.py migrate
 /usr/bin/python /opt/pcapoptikon/manage.py runserver 0.0.0.0:8000 >/var/log/pcapoptikon_web.log 2>&1 &
 /usr/bin/python /opt/pcapoptikon/manage.py run_daemon >/var/log/pcapoptikon_daemon.log 2>&1 &
 echo "Running on: http://"$(hostname -i)":8000"
+while true; do sleep 1; done
