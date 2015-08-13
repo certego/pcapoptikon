3af25c768e2ec6bc1d309a1c3c4d553ec4325ee7
Pietro Delsante <pietro.delsante@gmail.com>
2015-08-13 18:30:06 +0200

    Better start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+service mysql start
 /usr/bin/suricata -c /etc/suricata/suricata.yaml --unix-socket >/dev/null 2>&1 &
-/usr/bin/python /opt/pcapoptikon/manage.py runserver 0.0.0.0:8000 >/dev/null 2>&1 &
-/usr/bin/python /opt/pcapoptikon/manage.py run_daemon >/dev/null 2>&1
+/usr/bin/python /opt/pcapoptikon/manage.py runserver 0.0.0.0:8000 >/var/log/pcapoptikon_web.log 2>&1 &
+/usr/bin/python /opt/pcapoptikon/manage.py run_daemon >/var/log/pcapoptikon_daemon.log 2> &1 &
+echo Running on: http://$(hostname -i):8000
