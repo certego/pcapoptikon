26ffba878b124a86a3c1b2714472651afdba06f9
Pietro Delsante <pietro.delsante@gmail.com>
2015-08-13 18:46:27 +0200

    Mysql should be on when migrating

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+service mysql start
 cd /opt/pcapoptikon
 git pull origin master
 python manage.py migrate
-service mysql start
 /usr/bin/suricata -c /etc/suricata/suricata.yaml --unix-socket >/dev/null 2>&1 &
 /usr/bin/python /opt/pcapoptikon/manage.py runserver 0.0.0.0:8000 >/var/log/pcapoptikon_web.log 2>&1 &
 /usr/bin/python /opt/pcapoptikon/manage.py run_daemon >/var/log/pcapoptikon_daemon.log 2>&1 &
