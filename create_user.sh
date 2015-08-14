0ad8dfe2a56cc663659a054497eec4de1deda1dd
Pietro Delsante <pietro.delsante@gmail.com>
2015-08-14 17:18:48 +0200

    Removing ugly expect file and using python instead

--- create_user.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/expect -f
-spawn python manage.py createsuperuser --username admin --email user@example.com
-expect "Password: "
-send "admin\r"
-expect "Password (again): "
-send "admin\r"
