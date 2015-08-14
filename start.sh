7f1383d40486dfcb010b49cbf7d8630372e2ca40
Pietro Delsante <pietro.delsante@gmail.com>
2015-08-14 09:32:24 +0200

    Run oinkmaster at startup and every 24 hours, add some comments

--- start.sh
+++ start.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
+
+# Start MySQL server
 service mysql start
+
+# Ensure you got the latest pcapoptikon version
 cd /opt/pcapoptikon
 git pull origin master
 python manage.py migrate
+
+# Update suricata rules
+oinkmaster -C /etc/oinkmaster.conf -o /etc/suricata/rules
+
+# Start Suricata
 /usr/bin/suricata -c /etc/suricata/suricata.yaml --unix-socket >/dev/null 2>&1 &
+
+# Start the pcapoptikon HTTP server
 /usr/bin/python /opt/pcapoptikon/manage.py runserver 0.0.0.0:8000 >/var/log/pcapoptikon_web.log 2>&1 &
+
+# Start the pcapoptikon daemon
 /usr/bin/python /opt/pcapoptikon/manage.py run_daemon >/var/log/pcapoptikon_daemon.log 2>&1 &
-echo "Running on: http://"$(hostname -i)":8000"
-while true; do sleep 1; done
+
+# Give a hint about how to use
+echo "Running on: http://"$(hostname -i)":8000/"
+
+# Run oinkmaster every 24 hours and send SIGUSR2 to Suricata to make it reload the rules
+while true; do
+    sleep 60 * 60 * 24
+    oinkmaster -C /etc/oinkmaster.conf -o /etc/suricata/rules
+    killall -SIGUSR2 suricata
+done
