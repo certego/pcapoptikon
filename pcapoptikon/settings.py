71f75de183c8a85dfdfcf905d7ffa2bb8456cbb0
Pietro Delsante <pietro.delsante@gmail.com>
2015-08-18 11:59:13 +0200

    Adding missing STATICFILES_DIRS

--- pcapoptikon/settings.py
+++ pcapoptikon/settings.py
@@ -103,6 +103,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 # Media
 MEDIA_ROOT = '/var/tmp/django'
