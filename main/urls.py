8386435d1eb93f42d325571a2d8628e04db91a2f
Pietro Delsante <pietro.delsante@gmail.com>
2015-08-18 12:12:43 +0200

    Fixing another variable name

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 from main import views
 
 urlpatterns = patterns('',
-    url(r'^task/(?P<asset_id>\d+)/$', views.task, name='task'),
+    url(r'^task/(?P<task_id>\d+)/$', views.task, name='task'),
     url(r'^new_task/$', views.new_task, name='new_task'),
     url(r'^$', views.tasks, name='tasks'),
 )
