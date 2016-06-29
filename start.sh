b85e468e32e40aa370b0711ab53e67f868ae5b9d
Pietro Delsante <p.delsante@certego.net>
2016-06-29 18:25:07 +0200

    Improving logging

--- start.sh
+++ start.sh
@@ -20,6 +20,10 @@
 #           www.certego.net
 #
 
+function log() {
+    echo "[$(date +'%y-%m-%d %H:%M:%S')] $@"
+}
+
 # Send all output (stdout + stderr) to a log file
 exec > >(tee /var/log/pcapoptikon_startup.log)
 exec 2>&1
@@ -31,11 +35,11 @@ mkdir -p /var/log/suricata
 mkdir -p /var/run/suricata
 
 # Start MySQL server
-echo "Starting MySQL..."
+log "Starting MySQL..."
 service mysql start
 
 # Ensure you got the latest pcapoptikon version
-echo "Pulling PCAPOptikon repo..."
+log "Pulling PCAPOptikon repo..."
 cd /opt/pcapoptikon
 git pull origin master
 python manage.py migrate
@@ -49,52 +53,65 @@ touch /etc/suricata/rules/local.rules
  - local.rules' /etc/suricata/suricata.yaml
 
 # ETPro format: https://rules.emergingthreats.net/<code>/suricata/etpro.rules.tar.gz
-if [ $(echo $1 | egrep '^[0-9]{16}$' | wc -l) -eq 1 ]; then
-    echo "Using supplied OinkCode to set up ETPro ruleset in oinkmaster"
+if [ $(log $1 | egrep '^[0-9]{16}$' | wc -l) -eq 1 ]; then
+    log "Using supplied OinkCode to set up ETPro ruleset in oinkmaster"
     sed -ir "s|^url = http://rules.emergingthreats.net/open/suricata/emerging.rules.tar.gz|url = https://rules.emergingthreats.net/$1/suricata/etpro.rules.tar.gz|" /etc/oinkmaster.conf
 else
-    echo "Using default ET Open ruleset (to change this, please pass your ETPro oinkcode as the first param to $0"
+    log "Using default ET Open ruleset (to change this, please pass your ETPro oinkcode as the first param to $0"
 fi
 
 # Update suricata rules
-echo "Running OinkMaster to update signatures..."
+log "Running OinkMaster to update signatures..."
 oinkmaster -C /etc/oinkmaster.conf -o /etc/suricata/rules
 
 # Update sid-msg.map
-echo "Updating sid-msg.map"
+log "Updating sid-msg.map"
 /usr/share/oinkmaster/create-sidmap.pl /etc/suricata/rules > /etc/suricata/rules/sid-msg.map
 
 # Start Suricata
-echo "Starting Suricata..."
+log "Starting Suricata..."
 rm -f /var/run/suricata.pid
 /usr/bin/suricata -c /etc/suricata/suricata.yaml --unix-socket --pidfile /var/run/suricata.pid >/var/log/suricata/suricata.log 2>&1 &
 
 # Start the pcapoptikon HTTP server
-echo "Starting the PCAPOptikon HTTP Server..."
+log "Starting the PCAPOptikon HTTP Server..."
 /usr/bin/python /opt/pcapoptikon/manage.py runserver 0.0.0.0:8000 >/var/log/pcapoptikon_web.log 2>&1 &
-echo $! > /var/run/pcapoptikon-http.pid
+log $! > /var/run/pcapoptikon-http.pid
 
 # Start the pcapoptikon daemon
-echo "Starting the PCAPOptikon worker daemon..."
+log "Starting the PCAPOptikon worker daemon..."
 /usr/bin/python /opt/pcapoptikon/manage.py run_daemon >/var/log/pcapoptikon_daemon.log 2>&1 &
-echo $! > /var/run/pcapoptikon-daemon.pid
+log $! > /var/run/pcapoptikon-daemon.pid
 
 # Give a hint about how to use
-echo "Running on: http://"$(hostname -i)":8000/"
-echo "Username: admin"
-echo "Api-Key: "$(mysql -e 'SELECT `key` FROM tastypie_apikey WHERE user_id = (SELECT id FROM auth_user WHERE username = '"'"'admin'"'"');' pcapoptikon)
+log "Running on: http://"$(hostname -i)":8000/"
+log "Username: admin"
+log "Api-Key: "$(mysql -e 'SELECT `key` FROM tastypie_apikey WHERE user_id = (SELECT id FROM auth_user WHERE username = '"'"'admin'"'"');' pcapoptikon)
 
 # Run oinkmaster every 24 hours and restart suricata and pcapoptikon daemon to reload the rules
 while true; do
     sleep $(expr 60 \* 60 \* 24)
+
+    log "Running oinkmaster to update rules"
     oinkmaster -C /etc/oinkmaster.conf -o /etc/suricata/rules
+
+    log "Updating sid-msg.map"
+    /usr/share/oinkmaster/create-sidmap.pl /etc/suricata/rules > /etc/suricata/rules/sid-msg.map
+
+    log "Stopping pcapoptikon's run_daemon and deleting pid file"
     kill $(cat /var/run/pcapoptikon-daemon.pid)
     sleep 5
     rm -f /var/run/pcapoptikon-daemon.pid
+
+    log "Stopping suricata and deleting pid file"
     kill $(cat /var/run/suricata.pid)
     sleep 5
     rm -f /var/run/suricata.pid
+
+    log "Starting up suricata and run_daemon again"
     /usr/bin/suricata -c /etc/suricata/suricata.yaml --unix-socket --pidfile /var/run/suricata.pid >/dev/null 2>&1 &
     /usr/bin/python /opt/pcapoptikon/manage.py run_daemon >/var/log/pcapoptikon_daemon.log 2>&1 &
-    echo $! > /var/run/pcapoptikon-daemon.pid
+    log $! > /var/run/pcapoptikon-daemon.pid
+
+    log "Signatures reloaded"
 done
